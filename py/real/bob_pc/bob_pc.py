e66d513b66972e2df78dd2e28530f97cdeb55d18
gabiga7 <gabelga91@gmail.com>
2023-06-10 14:53:15 +0200

    finito

--- py/real/bob_pc/bob_pc.py
+++ py/real/bob_pc/bob_pc.py
@@ -13,18 +13,22 @@ def generate_public_key():
         # If it exists, read it and return the public key as a string
         with open(public_key_file, 'r') as f:
             public_key_data = f.read().replace('\n', '')
+            print("\033[34m" + "Public key successfully loaded from file." + "\033[0m")  # Blue
             return public_key_data
     else:
         # If it does not exist, generate a new RSA key pair
+        print("\033[36m" + "Generating a new RSA key pair..." + "\033[0m")  # Cyan
         public_key, private_key = newkeys(2048)
 
         # Save the public key to a file
         with open(public_key_file, 'w') as f:
             f.write(public_key.save_pkcs1().decode().replace('\n', ''))
+        print("\033[32m" + "Public key saved to file." + "\033[0m")  # Green
 
         # Save the private key to a file
         with open(private_key_file, 'w') as f:
             f.write(private_key.save_pkcs1().decode().replace('\n', ''))
+        print("\033[32m" + "Private key saved to file." + "\033[0m")  # Green
 
         # Return the public key as a string
         return public_key.save_pkcs1().decode().replace('\n', '')
@@ -36,11 +40,13 @@ def send_public_key_file(server_port):
     # Connect to the server
     server_address = ('localhost', server_port)
     client_socket.connect(server_address)
+    print("\033[33m" + f"Connected to the server at {server_address}" + "\033[0m")  # Yellow
 
     # Send the public key file content as UTF-8 string to the server
     with open("id.pub", "r") as file:
         public_key_file_content = file.read().replace('\n', '').encode('utf-8')
         client_socket.send(public_key_file_content)
+        print("\033[35m" + "Public key sent to server." + "\033[0m")  # Magenta
 
     return client_socket
 
@@ -48,10 +54,10 @@ def send_operation(client_socket):
     # Send an empty string to ask for permissions
     client_socket.send("".encode())
     response = client_socket.recv(4096).decode()
-    
+
     # Check the server response
     if response == 'ok':
-        print(f"{response}")
+        print("\033[32m" + f"Access granted by server. Ready to perform operations." + "\033[0m")  # Green
         while True:
             operation = input("Enter operation (or 'exit' to quit): ")
             if operation.lower() == 'exit':
@@ -60,9 +66,11 @@ def send_operation(client_socket):
             client_socket.send(operation.encode())
             response = client_socket.recv(4096).decode()
             print(f"Response: {response}")
+    elif response == 'ac ok':
+        print("\033[34m" + "AC created." + "\033[0m")  # Blue
     else:
-        print('Access denied.')
-        
+        print("\033[31m" + 'Access denied.' + "\033[0m")  # Red
+
 # Example usage
 if __name__ == "__main__":
     if len(sys.argv) != 2:
